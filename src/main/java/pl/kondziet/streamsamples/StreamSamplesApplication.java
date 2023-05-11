b23f86fdef8ce2062ce82cffb4377f397221117e
kondziet <konrad.dabrowski54@gmail.com>
2023-05-11 17:24:41 +0200

    Initial commit

---
+++ src/main/java/pl/kondziet/streamsamples/StreamSamplesApplication.java
@@ -0,0 +1,13 @@
+package pl.kondziet.streamsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamSamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kondziet/streamsamples/StreamSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kondziet.streamsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

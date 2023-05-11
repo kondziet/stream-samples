4ebbe5e7a695e1490c947bd2818aded2c57b7ace
kondziet <konrad.dabrowski54@gmail.com>
2023-05-11 17:31:33 +0200

    Create basic security configuration

---
+++ src/main/java/pl/kondziet/streamsamples/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package pl.kondziet.streamsamples.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
+
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers("/api/authentication/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        );
+
+        return httpSecurity.build();
+    }
+}

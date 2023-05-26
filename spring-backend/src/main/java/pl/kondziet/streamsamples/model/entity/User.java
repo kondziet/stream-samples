0c66f14e70cef9ad8d4fdf371f23751a9b44db35
kondziet <konrad.dabrowski54@gmail.com>
2023-05-26 11:15:07 +0200

    Use lombok instead of manual getters and setters

--- spring-backend/src/main/java/pl/kondziet/streamsamples/model/entity/User.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/model/entity/User.java
@@ -1,7 +1,15 @@
 package pl.kondziet.streamsamples.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -12,36 +20,4 @@ public class User {
     private String username;
     private boolean active;
 
-    public User(Long id, String username, boolean active) {
-        this.id = id;
-        this.username = username;
-        this.active = active;
-    }
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }

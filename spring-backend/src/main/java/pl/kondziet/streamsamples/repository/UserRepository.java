371937bab73b48de65f1849ec583597c1a8a1dcb
kondziet <konrad.dabrowski54@gmail.com>
2023-05-26 11:16:00 +0200

    Change identity data type

--- spring-backend/src/main/java/pl/kondziet/streamsamples/repository/UserRepository.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/repository/UserRepository.java
@@ -3,5 +3,5 @@ package pl.kondziet.streamsamples.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kondziet.streamsamples.model.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }

8e001d5368ed910cee98ab57751ecdfca4eb1305
kondziet <konrad.dabrowski54@gmail.com>
2023-05-30 14:23:35 +0200

    Refactor authentication controller

--- spring-backend/src/main/java/pl/kondziet/streamsamples/controller/AuthenticationController.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/controller/AuthenticationController.java
@@ -1,15 +1,29 @@
 package pl.kondziet.streamsamples.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.kondziet.streamsamples.model.DTO.AuthenticationRequest;
+import pl.kondziet.streamsamples.model.DTO.AuthenticationResponse;
+import pl.kondziet.streamsamples.model.DTO.SignupRequest;
+import pl.kondziet.streamsamples.service.AuthenticationService;
 
+@AllArgsConstructor
 @RestController()
 @RequestMapping("/api/authentication")
 public class AuthenticationController {
 
-    @GetMapping("/signup")
-    public String signup() {
-        return "allowed";
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthenticationResponse> signup(@RequestBody SignupRequest signupRequest) {
+        return ResponseEntity.ok(authenticationService.register(signupRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authentication(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
     }
 }

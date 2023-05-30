a5b923afe135abb2b2760d22f2478e8629cfcbfe
kondziet <konrad.dabrowski54@gmail.com>
2023-05-30 14:25:12 +0200

    Update initial data creation

--- spring-backend/src/main/java/pl/kondziet/streamsamples/util/DataLoader.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/util/DataLoader.java
@@ -1,32 +1,36 @@
 package pl.kondziet.streamsamples.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.kondziet.streamsamples.model.entity.User;
+import pl.kondziet.streamsamples.model.enums.Role;
 import pl.kondziet.streamsamples.repository.UserRepository;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public DataLoader(UserRepository userRepository) {
+    public DataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
         User user1 = User.builder()
                 .nickname("michaelScott")
-                .active(false)
-                .build();
-
-        User user2 = User.builder()
-                .nickname("dwightSchrute")
-                .active(false)
+                .email("example")
+                .password(passwordEncoder.encode("example"))
+                .role(Role.USER)
+                .active(true)
                 .build();
 
         userRepository.save(user1);
-        userRepository.save(user2);
     }
 }

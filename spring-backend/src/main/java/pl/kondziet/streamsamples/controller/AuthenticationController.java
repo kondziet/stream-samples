3d4a020510729fbf30459eb08e06856117e3e217
kondziet <konrad.dabrowski54@gmail.com>
2023-06-20 12:43:56 +0200

    Fix typo

--- spring-backend/src/main/java/pl/kondziet/streamsamples/controller/AuthenticationController.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/controller/AuthenticationController.java
@@ -1,13 +1,11 @@
 package pl.kondziet.streamsamples.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.kondziet.streamsamples.model.DTO.AuthenticationRequest;
 import pl.kondziet.streamsamples.model.DTO.AuthenticationResponse;
-import pl.kondziet.streamsamples.model.DTO.SignupRequest;
+import pl.kondziet.streamsamples.model.DTO.RegisterRequest;
 import pl.kondziet.streamsamples.service.AuthenticationService;
 
 @AllArgsConstructor
@@ -17,9 +15,9 @@ public class AuthenticationController {
 
     private AuthenticationService authenticationService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<AuthenticationResponse> signup(@RequestBody SignupRequest signupRequest) {
-        return ResponseEntity.ok(authenticationService.register(signupRequest));
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
     }
 
     @PostMapping("/authenticate")

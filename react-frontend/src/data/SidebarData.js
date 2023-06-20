2b7bb602ba0b9445b15ab55894666651dc630f4c
kondziet <konrad.dabrowski54@gmail.com>
2023-06-20 16:29:09 +0200

    Define stream categories to sidebar display

---
+++ react-frontend/src/data/SidebarData.js
@@ -0,0 +1,90 @@
+export const postCategoryData = [
+    {
+        id: 1,
+        label: "Regular posts",
+        value: "regular"
+    },
+    {
+        id: 2,
+        label: "Task solutions",
+        value: "solution"
+    },
+];
+
+export const streamCategoryData = [
+    {
+        id: 1,
+        label: "Filtering",
+        value: "filtering"
+    },
+    {
+        id: 2,
+        label: "Mapping",
+        value: "mapping"
+    },
+    {
+        id: 3,
+        label: "Sorting",
+        value: "sorting"
+    },
+    {
+        id: 4,
+        label: "Aggregation",
+        value: "aggregation"
+    },
+    {
+        id: 5,
+        label: "Matching",
+        value: "matching"
+    },
+    {
+        id: 6,
+        label: "Searching",
+        value: "searching"
+    },
+    {
+        id: 7,
+        label: "Limiting",
+        value: "limiting"
+    },
+    {
+        id: 8,
+        label: "Skipping",
+        value: "skipping"
+    },
+    {
+        id: 9,
+        label: "Distinct",
+        value: "distinct"
+    },
+    {
+        id: 10,
+        label: "Reducing",
+        value: "reducing"
+    },
+    {
+        id: 11,
+        label: "Collecting",
+        value: "collecting"
+    },
+    {
+        id: 12,
+        label: "Iterating",
+        value: "iterating"
+    },
+    {
+        id: 13,
+        label: "Generating",
+        value: "generating"
+    },
+    {
+        id: 14,
+        label: "Combining",
+        value: "combining"
+    },
+    {
+        id: 15,
+        label: "Parallel Processing",
+        value: "parallel"
+    }
+];

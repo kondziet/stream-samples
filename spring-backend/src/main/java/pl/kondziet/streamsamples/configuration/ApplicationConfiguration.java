011256bf51b0bc12c4eb71204ec72adfada87f0e
kondziet <konrad.dabrowski54@gmail.com>
2023-06-20 12:43:44 +0200

    Refactor beans configuration

---
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/configuration/ApplicationConfiguration.java
@@ -0,0 +1,26 @@
+package pl.kondziet.streamsamples.configuration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.kondziet.streamsamples.repository.UserRepository;
+
+@AllArgsConstructor
+@Configuration
+public class ApplicationConfiguration {
+
+    private UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username).get();
+            }
+        };
+    }
+}
--- spring-backend/src/main/java/pl/kondziet/streamsamples/security/AuthConfiguration.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/security/AuthConfiguration.java
@@ -1,39 +1,21 @@
-package pl.kondziet.streamsamples.configuration;
+package pl.kondziet.streamsamples.security;
 
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import pl.kondziet.streamsamples.repository.UserRepository;
 
-@AllArgsConstructor
 @Configuration
-public class ApplicationConfig {
-
-    private UserRepository userRepository;
-
+public class AuthConfiguration {
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return userRepository.findByEmail(username).get();
-            }
-        };
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
     }

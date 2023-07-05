bb48c75dec808688b7a2de1f3822a7eaca2b16e6
kondziet <konrad.dabrowski54@gmail.com>
2023-07-05 12:25:19 +0200

    Create sidebar component

--- react-frontend/src/components/Sidebar.js
+++ react-frontend/src/components/Sidebar.js
@@ -1,49 +1,49 @@
-import { useState } from "react";
-import { streamCategoryData, postCategoryData } from "../data/SidebarData";
-import { GoChevronLeft, GoChevronDown } from "react-icons/go";
+import React, { useState } from "react";
+import { 
+  AiOutlineClose, 
+  AiOutlineMenu, 
+  AiOutlineHome
+} from "react-icons/ai";
 
-function Sidebar() {
-    const [expandedIndex, setExpandedIndex] = useState(-1);
+import {
+  CgProfile
+} from "react-icons/cg"
+import { Link } from "react-router-dom";
 
-    const handleLabelClick = (clickedIndex) => {
-        setExpandedIndex((currentExpandedIndex) => {
-            if (currentExpandedIndex === clickedIndex) {
-                return -1;
-            }
-            return clickedIndex;
-        });
-    };
+function Side() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    const renderedStreamCategories = streamCategoryData.map((postCategory, index) => {
-        const expanded = index === expandedIndex;
-        const icon = (<span>{expanded ? <GoChevronDown /> : <GoChevronLeft />}</span>);
+  const handleSidebarClick = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
-        const renderedPostCategories = postCategoryData.map((postCategory) => {
-            
-            return(
-                <div className="border-b p-5" key={postCategory.id}>
-                    <div className="cursor-pointer">{postCategory.label}</div>
-                </div>
-            );
-        });
-
-        return(
-            <div key={postCategory.id}>
-                <div className="flex items-center justify-between bg-gray-300 p-3 border-b cursor-pointer" onClick={() => handleLabelClick(index)}>{postCategory.label}{icon}</div>
-                {expanded && (
-                    <div>
-                        {renderedPostCategories}
-                    </div>
-                )}
-            </div>
-        );
-    });
-
-    return(
-        <div className="w-1/4">
-            {renderedStreamCategories}
+  return(
+    <div className="flex justify-between items-center h-20 max-w-[1240px] mx-auto px-4">
+      <h1 className="text-3xl font-bold text-[#14551f]">Stream Samples</h1>
+        <div className="hidden md:flex">
+          Search
         </div>
-    );
+        <ul className="hidden md:flex">
+          <li>
+            <Link to={"/"} className="flex gap-2 items-center p-4">
+              <AiOutlineHome size={20} />Home
+            </Link>
+          </li>
+          <li>
+            <Link to={"/profile"} className="flex gap-2 items-center p-4">
+              <CgProfile size={20} />Profile
+            </Link>
+          </li>
+        </ul>
+          <div onClick={handleSidebarClick} className="block md:hidden">
+            {sidebarOpen ? <AiOutlineClose size={20} className="cursor-pointer" /> : <AiOutlineMenu size={20} className="cursor-pointer" />}
+          </div>
+        <ul className={sidebarOpen ? "fixed left-0 top-20 w-[100%] h-full border-r ease-in-out duration-500 md:hidden" : "ease-in-out duration-500 fixed left-[-100%] top-20"}>
+          <li className="flex gap-2 items-center p-4 border-b "><AiOutlineHome size={20} />Home</li>
+          <li className="flex gap-2 items-center p-4 border-b "><CgProfile size={20} />Profile</li>
+        </ul>
+    </div>
+  );
 }
 
-export default Sidebar;
+export default Side;

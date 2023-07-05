d3befbb9978dd2d17cf74dc42881dedcf11d61e7
kondziet <konrad.dabrowski54@gmail.com>
2023-07-05 12:23:41 +0200

    Add react router

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,18 @@
+import { Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import Profile from "./pages/Profile";
 
 function App() {
 
   return (
     <div className="App">
       <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }

805d2a9ad87d24e1755fb3adda35260545e26853
kondziet <konrad.dabrowski54@gmail.com>
2023-07-05 12:19:08 +0200

    Fix typo

--- spring-backend/src/main/java/pl/kondziet/streamsamples/model/repository/UserRepository.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/model/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package pl.kondziet.streamsamples.repository;
+package pl.kondziet.streamsamples.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.kondziet.streamsamples.model.entity.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }

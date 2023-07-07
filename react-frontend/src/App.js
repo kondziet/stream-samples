da33e774bf4bb418e5b4c1a5f420f871c927304a
kondziet <konrad.dabrowski54@gmail.com>
2023-07-07 14:09:43 +0200

    Add new routes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,9 @@
 import { Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
 import Profile from "./pages/Profile";
+import Posts from "./pages/Posts";
+import Login from "./pages/Login";
 
 function App() {
 
@@ -10,8 +11,10 @@ function App() {
     <div className="App">
       <Sidebar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Posts />} />
+        <Route path="/categories" element={<Posts />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );

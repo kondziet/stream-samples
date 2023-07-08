7c76ba52543ac29ce589beecd024e6b5b2138018
kondziet <konrad.dabrowski54@gmail.com>
2023-07-08 07:50:27 +0200

    Add authenticated flag to authentication context

--- react-frontend/src/context/AuthenticationContext.js
+++ react-frontend/src/context/AuthenticationContext.js
@@ -1,13 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthenticationContext = createContext();
 
 function AuthenticationProvider({ children }) {
 
     const [authentication, setAuthentication] = useState({});
+    const [userAuthenticated, setUserAuthenticated] = useState(false);
+
+    useEffect(() => {
+        setUserAuthenticated(!!authentication.accessToken);
+    }, [authentication]);
 
     return(
-        <AuthenticationContext.Provider value={{ authentication, setAuthentication }}>
+        <AuthenticationContext.Provider value={{ authentication, setAuthentication, userAuthenticated }}>
             {children}
         </AuthenticationContext.Provider>
     );

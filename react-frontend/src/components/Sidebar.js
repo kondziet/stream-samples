c2d91e0b0de0ba2ef0dcb07fce659d99c9e7ada6
kondziet <konrad.dabrowski54@gmail.com>
2023-07-08 07:47:48 +0200

    Add logout

--- react-frontend/src/components/Sidebar.js
+++ react-frontend/src/components/Sidebar.js
@@ -8,13 +8,28 @@ import {
 import {
   CgProfile
 } from "react-icons/cg"
+
+import {
+  TbCategory
+} from "react-icons/tb"
+
+import {
+  BiLogOut
+} from "react-icons/bi"
+
 import { Link } from "react-router-dom";
+import useAuthenticationContext from "../hooks/useAuthentication";
 
-function Side() {
+function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { userAuthenticated, setAuthentication } = useAuthenticationContext();
 
-  const handleSidebarClick = () => {
+  const handleSidebarToggle = () => {
     setSidebarOpen(!sidebarOpen);
+  };
+
+  const handleSidebarLogout = () => {
+    setAuthentication({});
   };
 
   return(
@@ -30,20 +45,34 @@ function Side() {
             </Link>
           </li>
           <li>
-            <Link to={"/profile"} className="flex gap-2 items-center p-4">
+            <Link to={"/categories"} className="flex gap-2 items-center p-4">
+              <TbCategory size={20} />Categories
+            </Link>
+          </li>
+          <li>
+            <Link to={"/login"} className="flex gap-2 items-center p-4">
               <CgProfile size={20} />Profile
             </Link>
           </li>
+          
+          { userAuthenticated ? (
+            <li>
+              <Link onClick={handleSidebarLogout} to={"/logout"} className="flex gap-2 items-center p-4">
+                <BiLogOut size={20} />Logout
+              </Link>
+            </li>
+          ) : (<div></div>)}
         </ul>
-          <div onClick={handleSidebarClick} className="block md:hidden">
+          <div onClick={handleSidebarToggle} className="block md:hidden">
             {sidebarOpen ? <AiOutlineClose size={20} className="cursor-pointer" /> : <AiOutlineMenu size={20} className="cursor-pointer" />}
           </div>
-        <ul className={sidebarOpen ? "fixed left-0 top-20 w-[100%] h-full border-r ease-in-out duration-500 md:hidden" : "ease-in-out duration-500 fixed left-[-100%] top-20"}>
+        <ul className={sidebarOpen ? "bg-white fixed left-0 top-20 w-[100%] h-full border-r ease-in-out duration-500 md:hidden" : "ease-in-out duration-500 fixed left-[-100%] top-20"}>
           <li className="flex gap-2 items-center p-4 border-b "><AiOutlineHome size={20} />Home</li>
+          <li className="flex gap-2 items-center p-4 border-b "><TbCategory size={20} />Categories</li>
           <li className="flex gap-2 items-center p-4 border-b "><CgProfile size={20} />Profile</li>
         </ul>
     </div>
   );
 }
 
-export default Side;
+export default Sidebar;

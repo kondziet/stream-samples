ac6bdb568ac4a527cbb54d8b3409eaf93527d9df
kondziet <konrad.dabrowski54@gmail.com>
2023-07-18 17:33:41 +0200

    Expand post dto

--- spring-backend/src/main/java/pl/kondziet/streamsamples/model/DTO/PostPOST.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/model/DTO/PostPOST.java
@@ -6,10 +6,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.kondziet.streamsamples.model.entity.User;
+import pl.kondziet.streamsamples.model.enums.PostCategory;
 
 @Data
 public class PostPOST {
 
     private String title;
+    private String description;
+    private PostCategory postCategory;
     private String code;
 }

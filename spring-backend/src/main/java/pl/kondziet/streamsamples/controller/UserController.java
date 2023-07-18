b151e9343b7ad87e1f5dc985c1eea910816b18da
kondziet <konrad.dabrowski54@gmail.com>
2023-07-18 17:31:36 +0200

    Remove unnecessary endpoints

--- spring-backend/src/main/java/pl/kondziet/streamsamples/controller/UserController.java
+++ spring-backend/src/main/java/pl/kondziet/streamsamples/controller/UserController.java
@@ -19,33 +19,5 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserRepository userRepository;
-    private PostRepository postRepository;
 
-    @GetMapping()
-    public ResponseEntity<List<User>> get() {
-        return ResponseEntity.ok(userRepository.findAll());
-    }
-
-    @PostMapping
-    @Transactional
-    public ResponseEntity<String> post(Authentication authentication, @RequestBody PostPOST newPost) {
-        User user = userRepository.findAll().stream()
-                .filter(u -> u.getEmail().equals(authentication.getName()))
-                .findAny()
-                .orElse(null);
-
-        Post post = Post.builder()
-                .title(newPost.getTitle())
-                .code(newPost.getCode())
-                .author(user)
-                .build();
-
-        postRepository.save(post);
-
-        assert user != null;
-        user.getPosts().add(post);
-
-        return ResponseEntity.ok("Created");
-    }
 }

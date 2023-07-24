2b75f971c2bb19bfcc71bfc11b4f431c9ca5ec5e
kondziet <konrad.dabrowski54@gmail.com>
2023-07-25 00:21:10 +0200

    Remove register form styling

--- react-frontend/src/pages/Register.js
+++ react-frontend/src/pages/Register.js
@@ -43,20 +43,18 @@ function Register() {
 
     return(
         <div className="w-screen flex-grow max-w-[1240px] flex justify-center">
-            <div className="flex flex-col items-center w-96 h-96 border rounded-xl border-black mt-20 md:mt-44">
+            <div>
                 <form onSubmit={handleSubmit} className="p-4 m-2">
                     <label>Email:</label>
-                    <input value={email} onChange={(event) => setEmail(event.target.value)}  className="w-full px-4 py-2 border border-gray-500 rounded-md focus:outline-none focus:border-green-300" type="text" required />
+                    <input value={email} onChange={(event) => setEmail(event.target.value)} className="border border-black rounded" type="text" required />
                     <label>Password:</label>
-                    <input value={password} onChange={(event) => setPassword(event.target.value)} className="w-full px-4 py-2 border border-gray-500 rounded-md focus:outline-none focus:border-green-300" type="password" required />
+                    <input value={password} onChange={(event) => setPassword(event.target.value)} className="border border-black rounded" type="password" required />
                     <label>Confirm password:</label>
-                    <input value={repeatedPassword} onChange={(event) => setRepeatedPassword(event.target.value)} className="w-full px-4 py-2 border border-gray-500 rounded-md focus:outline-none focus:border-green-300" type="password" required />
+                    <input value={repeatedPassword} onChange={(event) => setRepeatedPassword(event.target.value)} className="border border-black rounded" type="password" required />
                     <label>Nickname:</label>
-                    <input value={nickname} onChange={(event) => setNickname(event.target.value)} className="w-full px-4 py-2 border border-gray-500 rounded-md focus:outline-none focus:border-green-300" type="text" required />
-                    <div className="flex items-center justify-between mt-6">
-                        <button className="rounded-b-xl bg-green-400 px-8 py-2">Register</button>
-                        <p className={errorMsg ? "font-bold" : "hidden"}>{errorMsg}</p>
-                    </div>
+                    <input value={nickname} onChange={(event) => setNickname(event.target.value)} className="border border-black rounded" type="text" required />
+                    <button className="border border-black rounded">Register</button>
+                    <p className={errorMsg ? "font-bold" : "hidden"}>{errorMsg}</p>
                 </form>
             </div>
         </div>
